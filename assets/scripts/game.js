b50dcf5195e1e53adae1129b9af7885c6904fac0
Aansteydev <>
2023-11-09 13:32:58 +0000

    add initial JS

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,1 +1,20 @@
+img = new Image()
+img.src = "assets/images/question.png"
+img.addEventListener(load,function(){
+    //img loaded
+});
+
+img = new Image();
+img.src = "assets/images/cross.png";
+img.addEventListener(load, function () {
+    //img loaded
+});
+
+img = new Image();
+img.src = "assets/images/circle.png";
+img.addEventListener(load, function () {
+    //img loaded
+});
+
+document.querySelector('#btnstart').addEventListener('click,spin');
 
